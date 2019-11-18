9ac73b88274780cfd99a05a3e581808f629a0960
LukyVj <lucas.bonomi@gmail.com>
2019-11-18 18:17:48 +0100

    improve

--- index.js
+++ index.js
@@ -1,50 +1,52 @@
-const fs = require('fs');
-const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const fs = require("fs");
+const Discord = require("discord.js");
+const { prefix, token } = require("./config.json");
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter(file => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
 }
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!\nI am ready !`);;
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!\nI am ready !`);
 
-  client.user.setActivity('madamnazar.io');
+  client.user.setActivity("madamnazar.io");
 });
 
-client.on('message', message => {
+client.on("message", message => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
-  
+
   console.log(message.content);
 
   const args = message.content.slice(prefix.length).split(/ +/);
-	const commandName = args.shift().toLowerCase();
+  const commandName = args.shift().toLowerCase();
 
   if (!client.commands.has(commandName)) return;
 
   const command = client.commands.get(commandName);
 
   if (command.args && !args.length) {
-		let reply = `You didn't provide any arguments, ${message.author}!`;
+    let reply = `You didn't provide any arguments, ${message.author}!`;
 
-		if (command.usage) {
-			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
-		}
+    if (command.usage) {
+      reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+    }
 
-		return message.channel.send(reply);
-	}
-  
+    return message.channel.send(reply);
+  }
+
   try {
     command.execute(message, args);
   } catch (error) {
     console.error(error);
-    message.reply('there was an error trying to execute that command!');
+    message.reply("there was an error trying to execute that command!");
   }
 });
 
