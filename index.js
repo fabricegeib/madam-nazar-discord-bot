bd5a1d614194a2f10617ae94db52ef491fef66c4
LukyVj <lucas.bonomi@gmail.com>
2019-11-18 19:49:53 +0100

    Update

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const Discord = require("discord.js");
 const { prefix, token } = require("./config.json");
 
-
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
