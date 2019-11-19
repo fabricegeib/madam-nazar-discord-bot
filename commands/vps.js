41474b2cbf427f44ac57f8085c7e0b0fce8526ea
Fabrice Geib <fabricegeib@gmail.com>
2019-11-19 22:44:02 +0100

    update

--- commands/vps.js
+++ commands/vps.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'vps',
 	description: 'first command on vps',
 	execute(message, args) {
-		message.channel.send('Madam Nazar Bot - VPS');
+		message.channel.send('Madam Nazar Bot - VPS auto reload');
 	},
 };
